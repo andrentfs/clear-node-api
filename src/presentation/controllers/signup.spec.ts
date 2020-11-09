e8ce405dfb73e0bd54cde7805c039e25ce95d084
Andre Luiz <andre@awtecnologia.com.br>
2020-11-09 23:11:59 +0000

    test: ensure SingUpController returns 400 if no password is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -29,4 +29,32 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
+
+  test('Should return 400 if no password is provider', () => {
+    const sut = new SignUpControler()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@emai.com',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('password'))
+  })
+
+  test('Should return 400 if no passwordConfirmation is provider', () => {
+    const sut = new SignUpControler()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@emai.com',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'))
+  })
 })
