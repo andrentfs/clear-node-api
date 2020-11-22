1c7c5e8bc0692ab5ac6f74b776a49336e3edd359
Andre Luiz <andre@awtecnologia.com.br>
2020-11-22 02:33:41 +0000

    test: ensur DbAddAccount returns an account on sucess

--- src/data/usercases/add-acount/db-add-account.spec.ts
+++ src/data/usercases/add-acount/db-add-account.spec.ts
@@ -94,4 +94,20 @@ describe('DbAddAcount Usecase', () => {
     const promise = sut.add(accountData)
     await expect(promise).rejects.toThrow()
   })
+
+  test('Should return an account on sucess', async () => {
+    const { sut } = makeSut()
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+    const account = await sut.add(accountData)
+    expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'hashed_password'
+    })
+  })
 })
